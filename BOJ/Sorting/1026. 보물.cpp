ad0feacd62541804891f7cb2ffbefab1e6f9e158
eastgerm <dowk0331@gmail.com>
2019-11-03 15:55:11 +0900

    feat: 1026 풀이

---
+++ BOJ/Sorting/1026. 보물.cpp
@@ -0,0 +1,41 @@
+//
+// Created by kimdong on 2019-11-03.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+vector<int> A,B;
+
+bool compare(int a, int b) {return a > b;}
+
+void input();
+int item();
+void output();
+
+int main() {
+    input();
+    output();
+    return 0;
+}
+
+void input() {
+    cin >> N;
+    A.assign(N,0);
+    B.assign(N,0);
+    for(int i=0; i<N; i++) cin >> A[i];
+    for(int i=0; i<N; i++) cin >> B[i];
+}
+int item() {
+    int sum = 0;
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end(),compare);
+    for(int i=0; i<N; i++) sum += A[i]*B[i];
+    return sum;
+}
+void output() {
+    cout << item() << '\n';
+}
