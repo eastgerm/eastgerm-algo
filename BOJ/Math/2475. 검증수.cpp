25daca0d8b2c65c6633298a3c4c3c9e3e3841025
eastgerm <dowk0331@gmail.com>
2019-11-04 02:03:37 +0900

    feat: 2475 풀이

---
+++ BOJ/Math/2475. 검증수.cpp
@@ -0,0 +1,15 @@
+//
+// Created by kimdong on 2019-05-14.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d,e;
+    cin >> a >> b >> c >> d >> e;
+    int sum = a*a + b*b + c*c + d*d + e*
+                                      e;
+    cout << sum%10 << '\n';
+    return 0;
+}
