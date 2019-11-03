5a7e36eed466c042b5adc34e58ec29e531f11106
eastgerm <dowk0331@gmail.com>
2019-11-03 16:02:06 +0900

    feat: 1075 풀이

---
+++ BOJ/Math/1075. 나누기.cpp
@@ -0,0 +1,31 @@
+//
+// Created by kimdong on 2019-11-03.
+//
+
+#include <iostream>
+using namespace std;
+
+int N,F;
+
+void input();
+int result();
+void output();
+
+int main(){
+    input();
+    output();
+    return 0;
+}
+
+void input(){
+    cin >> N >> F;
+}
+int result(){
+    int start = N - N%100;
+    for(int i=start;;i++) if(i%F == 0) return i;
+}
+void output(){
+    int res = result();
+    int ten = (res%100)/10, one = res%10;
+    cout << ten << one << endl;
+}
