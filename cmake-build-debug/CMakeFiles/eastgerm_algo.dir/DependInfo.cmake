c2acb574ac24450ee93f1a4b5e8232021c124897
eastgerm <dowk0331@gmail.com>
2019-11-03 16:17:36 +0900

    feat: 빌드의 entry point는 1000번 문제로 설정

--- cmake-build-debug/CMakeFiles/eastgerm_algo.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/eastgerm_algo.dir/DependInfo.cmake
@@ -5,8 +5,6 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
   "/Users/kimdong/Desktop/eastgerm-algo/BOJ/Basic/1000. A+B.cpp" "/Users/kimdong/Desktop/eastgerm-algo/cmake-build-debug/CMakeFiles/eastgerm_algo.dir/BOJ/Basic/1000._A+B.cpp.o"
-  "/Users/kimdong/Desktop/eastgerm-algo/BOJ/Basic/1008. A divide B.cpp" "/Users/kimdong/Desktop/eastgerm-algo/cmake-build-debug/CMakeFiles/eastgerm_algo.dir/BOJ/Basic/1008._A_divide_B.cpp.o"
-  "/Users/kimdong/Desktop/eastgerm-algo/BOJ/Basic/1009. 분산처리.cpp" "/Users/kimdong/Desktop/eastgerm-algo/cmake-build-debug/CMakeFiles/eastgerm_algo.dir/BOJ/Basic/1009._분산처리.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- cmake-build-debug/CMakeFiles/eastgerm_algo.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/eastgerm_algo.dir/cmake_clean.cmake
@@ -1,7 +1,5 @@
 file(REMOVE_RECURSE
   "CMakeFiles/eastgerm_algo.dir/BOJ/Basic/1000._A+B.cpp.o"
-  "CMakeFiles/eastgerm_algo.dir/BOJ/Basic/1008._A_divide_B.cpp.o"
-  "CMakeFiles/eastgerm_algo.dir/BOJ/Basic/1009._분산처리.cpp.o"
   "eastgerm_algo.pdb"
   "eastgerm_algo"
 )
