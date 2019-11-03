f1a08c2b45ca5217af271546c8fa38fab3d605ce
eastgerm <dowk0331@gmail.com>
2019-11-04 01:20:56 +0900

    feat: 1934 풀이

---
+++ BOJ/Math/1934. 최소공배수.cpp
@@ -0,0 +1,38 @@
+//
+// Created by kimdong on 2018-05-02.
+//
+
+#include <iostream>
+using namespace std;
+
+#define MAX(x,y) ( x>y ? x : y )
+
+int main()
+{
+    int T = 0, a = 0, b = 0, start = 0;
+    cin >> T;
+
+    while (T--) {
+        cin >> a >> b;
+        start = MAX(a, b);
+        start /= 2;
+
+        int GCD = 1, LCM = 1;
+
+        if (a != b) {
+            for (int i = start; i > 0; i--) {
+                if ((a%i == 0) && (b%i == 0)) {
+                    GCD = i; break;
+                }
+            }
+        }
+        else
+            GCD = a;
+
+        LCM = (a*b) / GCD;
+
+        cout << LCM << endl;
+    }
+
+    return 0;
+}
