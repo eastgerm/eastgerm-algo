8d7d6d6d97c2b3dc6b1acf9a89d2a9466974097f
eastgerm <dowk0331@gmail.com>
2019-11-03 16:03:26 +0900

    feat: 1085 풀이

---
+++ BOJ/Math/1085. 직사각형에서 탈출.cpp
@@ -0,0 +1,33 @@
+//
+// Created by kimdong on 2019-11-03.
+//
+
+#include <iostream>
+using namespace std;
+
+#define MIN(x,y) ((x)<(y)?(x):(y))
+
+int x,y,w,h;
+
+void input();
+int minEdge();
+void output();
+
+int main() {
+    input();
+    output();
+
+    return 0;
+}
+
+void input(){
+    cin >> x >> y >> w >> h;
+}
+int minEdge(){
+    int horizon = MIN(x,w-x);
+    int vertical = MIN(y,h-y);
+    return MIN(horizon,vertical);
+}
+void output(){
+    cout<< minEdge() << endl;
+}
