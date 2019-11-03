a9ecedcbea080289677323c4415acfb68078b469
eastgerm <dowk0331@gmail.com>
2019-11-03 16:02:41 +0900

    feat: 1076 풀이

---
+++ BOJ/Math/1076. 저항.cpp
@@ -0,0 +1,54 @@
+//
+// Created by kimdong on 2019-11-03.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ten,one,exp;
+
+void input();
+long long makeNum(string s);
+long long makeExp(string s);
+void output();
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    input();
+    output();
+    return 0;
+}
+
+void input(){
+    cin >> ten >> one >> exp;
+}
+long long makeNum(string s){
+    if(s == "black") return 0;
+    else if(s == "brown") return 1;
+    else if(s == "red") return 2;
+    else if(s == "orange") return 3;
+    else if(s == "yellow") return 4;
+    else if(s == "green") return 5;
+    else if(s == "blue") return 6;
+    else if(s == "violet") return 7;
+    else if(s == "grey") return 8;
+    else if(s == "white") return 9;
+}
+long long makeExp(string s){
+    if(s == "black") return 1;
+    else if(s == "brown") return 10;
+    else if(s == "red") return 100;
+    else if(s == "orange") return 1000;
+    else if(s == "yellow") return 10000;
+    else if(s == "green") return 100000;
+    else if(s == "blue") return 1000000;
+    else if(s == "violet") return 10000000;
+    else if(s == "grey") return 100000000;
+    else if(s == "white") return 1000000000;
+}
+void output(){
+    long long ans = (makeNum(ten)*10 + makeNum(one)) * makeExp(exp);
+    cout << ans << '\n';
+}
