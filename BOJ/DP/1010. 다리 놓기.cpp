102087529ca59b24a3e58d9263ee75861ad2ee04
eastgerm <dowk0331@gmail.com>
2019-11-03 15:46:09 +0900

    feat: 1010 풀이

---
+++ BOJ/DP/1010. 다리 놓기.cpp
@@ -0,0 +1,54 @@
+//
+// Created by kimdong on 2019-11-03.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int T = 0;
+int N = 0;
+int M = 0;
+int dp[31][31] = { 0 };
+
+void inputT();
+void solve();
+void input();
+void makeDP();
+void output();
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    inputT();
+    makeDP();
+    solve();
+
+    return 0;
+}
+
+void inputT() {
+    cin >> T;
+}
+
+void makeDP() {
+    for (int i = 0; i <= 30; i++)
+        for (int j = 0; j <= i / 2; j++)
+            !j ? dp[i][j] = dp[i][i - j] = 1 : dp[i][j] = dp[i][i - j] = dp[i - 1][j - 1] + dp[i - 1][j];
+}
+
+void solve() {
+    for (int tc = 1; tc <= T; tc++) {
+        input();
+        output();
+    }
+}
+
+void input() {
+    cin >> N >> M;
+}
+
+void output() {
+    cout << dp[M][N] << endl;
+}
